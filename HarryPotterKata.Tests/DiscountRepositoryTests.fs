70c8c414df1010fdd0b9c314c910cc77b473ca78
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-14 20:36:49 +0000

    Added test for discount repository

---
+++ HarryPotterKata.Tests/DiscountRepositoryTests.fs
@@ -0,0 +1,12 @@
+﻿namespace HarryPotterKata.Tests
+open NUnit.Framework
+open HarryPotterKata
+
+type DiscountRepositoryTests() = 
+    let discountRepository = new DiscountRepository()
+
+    [<Test>]
+    member this.WhenCallingGetDiscountFor2Books() = 
+        let bookCount = 2
+        let result = discountRepository.GetDiscountFor(bookCount)
+        Assert.That(result, Is.EqualTo 5)
