cf74e346b3857918b7d170cec9310cb63976be38
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-14 20:48:39 +0000

    Added implementation for Discount repository

--- HarryPotterKata/DiscountRepository.fs
+++ HarryPotterKata/DiscountRepository.fs
@@ -3,4 +3,7 @@
 type DiscountRepository() = 
     member x.GetDiscountFor bookCount = 
         if bookCount = 2 then 5
+        else if bookCount = 3 then 10
+        else if bookCount = 4 then 20
+        else if bookCount = 5 then 25
         else 0
