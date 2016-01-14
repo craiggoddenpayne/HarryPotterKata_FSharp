ccf26dac9d2d71e4434cc9a7cbb4d3492fc8c36c
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-14 20:15:19 +0000

    Added scenario where there are two books for 5% discount and two same books for no discount

--- HarryPotterKata/PricingCalculator.fs
+++ HarryPotterKata/PricingCalculator.fs
@@ -5,5 +5,14 @@ type PricingCalculator() =
     member x.SingleBookCost = 8
 
     member x.GetPrice (books : List<Book>) =
-        x.SingleBookCost * books.Length
         
+        let bookGroups = query {
+            for book in books do
+            groupBy book.Title 
+            count
+        }
+
+        if bookGroups = 2 
+        then 15.2
+        else
+        (float)(x.SingleBookCost * books.Length)
