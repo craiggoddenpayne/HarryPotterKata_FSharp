b2ae89feee0a2d452dc27d4bcecd7d0f86434162
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-14 19:55:42 +0000

    Added test for two different books

--- HarryPotterKata.Tests/PricingCalculatorTests.fs
+++ HarryPotterKata.Tests/PricingCalculatorTests.fs
@@ -11,3 +11,9 @@ type PricingCalculatorTests() =
         let book = [new Book("Mock")]
         let result = calculator.GetPrice(book)
         Assert.That(result, Is.EqualTo 8)
+
+    [<Test>]
+    member this.WhenCallingGetPriceWithTwoDifferentBooksItShouldBe16() = 
+        let books = [new Book("Mock1");new Book("Mock2")]
+        let result = calculator.GetPrice(books)
+        Assert.That(result, Is.EqualTo 16)
