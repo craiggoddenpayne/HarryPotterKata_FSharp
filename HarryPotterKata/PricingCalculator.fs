3fdb36ee40250a0c571bdcbc29d07c0afb6ff96c
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-14 19:58:57 +0000

    Added implementation of singlebookcost

--- HarryPotterKata/PricingCalculator.fs
+++ HarryPotterKata/PricingCalculator.fs
@@ -1,6 +1,9 @@
 ﻿namespace HarryPotterKata
 
 type PricingCalculator() =
+
+    member x.SingleBookCost = 8
+
     member x.GetPrice (books : List<Book>) =
         if books.Length = 1 
         then 8
