e5666846035bbd23821586c048ccd0cf9ef559b9
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-14 20:00:24 +0000

    Added implementation of test for two books

--- HarryPotterKata/PricingCalculator.fs
+++ HarryPotterKata/PricingCalculator.fs
@@ -5,7 +5,5 @@ type PricingCalculator() =
     member x.SingleBookCost = 8
 
     member x.GetPrice (books : List<Book>) =
-        if books.Length = 1 
-        then x.SingleBookCost
-        else 
-        0 
+        x.SingleBookCost * books.Length
+        
