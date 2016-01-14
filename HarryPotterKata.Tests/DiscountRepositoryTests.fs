0b0c89766ef7a7415c1857cbab9d1a76a4801a2d
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-14 20:47:08 +0000

    Added tests for 3,4,5 books for discount repo

--- HarryPotterKata.Tests/DiscountRepositoryTests.fs
+++ HarryPotterKata.Tests/DiscountRepositoryTests.fs
@@ -6,7 +6,31 @@ type DiscountRepositoryTests() =
     let discountRepository = new DiscountRepository()
 
     [<Test>]
-    member this.WhenCallingGetDiscountFor2Books() = 
+    member this.WhenCallingGetDiscountFor1BookItShouldReturn0() = 
+        let bookCount = 1
+        let result = discountRepository.GetDiscountFor(bookCount)
+        Assert.That(result, Is.EqualTo 0)
+    
+    [<Test>]
+    member this.WhenCallingGetDiscountFor2BooksItShouldReturn5() = 
         let bookCount = 2
         let result = discountRepository.GetDiscountFor(bookCount)
         Assert.That(result, Is.EqualTo 5)
+
+    [<Test>]
+    member this.WhenCallingGetDiscountFor3BooksItShouldReturn10() = 
+        let bookCount = 3
+        let result = discountRepository.GetDiscountFor(bookCount)
+        Assert.That(result, Is.EqualTo 10)
+
+    [<Test>]
+    member this.WhenCallingGetDiscountFor4BooksItShouldReturn20() = 
+        let bookCount = 4
+        let result = discountRepository.GetDiscountFor(bookCount)
+        Assert.That(result, Is.EqualTo 20)
+    
+    [<Test>]
+    member this.WhenCallingGetDiscountFor5BooksItShouldReturn25() = 
+        let bookCount = 5
+        let result = discountRepository.GetDiscountFor(bookCount)
+        Assert.That(result, Is.EqualTo 25)
